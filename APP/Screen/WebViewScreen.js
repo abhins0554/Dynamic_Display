471ecf5818f23771e618967003811700ba6db644
Abhishek Tripathi <63142055+abhins0554@users.noreply.github.com>
2021-01-03 19:27:09 +0530

    new commit

--- APP/Screen/WebViewScreen.js
+++ APP/Screen/WebViewScreen.js
@@ -1,20 +1,68 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, StatusBar, FlatList } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  RefreshControl,
+  ScrollView,
+} from "react-native";
 import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
 
 interface WebViewScreenProp {
   route: RouteProp<RootStackParamList, "WebViewScreen">;
   navigation: StackNavigationProp<RootStackParamList, "WebViewScreen">;
 }
 
-let link = "http://15.207.254.111:5000/";
+function wait(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
 
 function WebViewScreen(props) {
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [link, set_link] = useState("http://15.207.254.111:5000/");
+  // const [link, set_link] = useState("http://google.com/");
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(200000).then(() => setRefreshing(false));
+  }, [refreshing]);
+
+  const webViewRef = React.useRef(null);
   return (
     <View style={styles.main}>
       <StatusBar />
       <View style={{ borderWidth: 2, width: "100%", height: "100%" }}>
-        <WebView source={{ uri: link }} />
+        <ScrollView
+          style={styles.ScrollStyle}
+          contentContainerStyle={{ flex: 1 }}
+          refreshControl={
+            <RefreshControl
+              refreshing={false}
+              onRefresh={() => {
+                console.log(webViewRef);
+                webViewRef.current.reload();
+                // link = "www.facebook.com";
+                // set_link("http://google.com/");
+
+                // set_link("http://15.207.254.111:5000/");
+              }} // exl in function : this.yourWebview.reload();
+            />
+          }
+        >
+          <WebView
+            stylestyle={styles.container}
+            ref={webViewRef}
+            mediaPlaybackRequiresUserAction={false}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            allowsInlineMediaPlayback={true}
+            source={{ uri: link }}
+          />
+        </ScrollView>
       </View>
     </View>
   );
@@ -27,6 +75,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flexDirection: "column",
     alignContent: "center",
+  },
+  container: {
+    position: "relative",
+  },
+  ScrollStyle: {
+    backgroundColor: "white",
+    position: "relative",
   },
 });
 export default WebViewScreen;
